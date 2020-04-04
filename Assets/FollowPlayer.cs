20c563d4ef1ab6da561aaa6bdd571f0b77c8a519
niole <niolenelson@gmail.com>
2020-04-04 13:44:26 -0700

    Camera follows behind player

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+
+    public Vector3 offset = new Vector3(0, 0, -8);
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // feed player position to the camera
+        // to make it follow the player, without spinning around with the player
+        transform.position = player.position + offset;
+        Debug.Log(player.position);
+    }
+}
